403da69fbbee833ff79d6ff251c179aee2e9f9fa
codegecko <johnugochi596@gmail.com>
2025-06-18 14:37:10 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const dreamHoliday = {
+  destination: "paris",
+  activity: "swim",
+  accommodation: "hotel",
+  companion: "husband",
+};
+const { destination, activity, accommodation, companion } = dreamHoliday;
+/*
+Challenge
+1. Complete the object dreamHoliday with whatever
+   information is true for you. Feel free to add
+   extra properties or change the existing ones.
+2. Destructure the object and use the individual
+   variables to log out one or more sentences about
+   your dream holiday.
+
+E.g. "I would love to go to Austin, Texas to visit the Tesla HQ.
+     I'd sleep in a luxury ranch and hang out with Elon Musk all day."
+*/
+console.log(
+  `1 would love to go to ${destination},to ${activity} with my ${companion} and lodge in their best ${accommodation}`
+);
