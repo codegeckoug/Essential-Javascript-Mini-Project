022cf5b909d267c2b586aa14da522465486db3ca
codegecko <johnugochi596@gmail.com>
2025-06-19 12:35:11 +0100

    update index.js
    Challenge 3: Arrow functions

--- index.js
+++ index.js
@@ -38,3 +38,64 @@ const getTheftAlert = function (numberOfTransactionsHour) {
 };
 
 console.log(getTheftAlert(6));
+
+//CHALLENGE THREE a: ARROW FUNCTION
+
+//function speedWarning(speed){
+//    return `You are going at ${speed} mph!`
+//}
+
+//console.log(speedWarning(40))
+
+/*
+Challenge
+1. Refactor this function to use an arrow function.
+2. Make sure you write the least code possible.
+*/
+
+const speedWarning = (speed) => `You are going at ${speed} mph!`;
+console.log(speedWarning(40));
+
+//CHALLENGE THREE b: ARROW FUNCTION WITH MULTIPLE PARAMETERS
+
+//const speedWarning = speed => `You are going at ${speed} mph!`
+
+//console.log(speedWarning(30, 40))
+
+/*
+Challenge
+1. Refactor this function so it only warns drivers
+   who are going over the speed limit.
+2. The function now needs to take in two parameters.
+   The first is the speed limit, the second is the
+   driver's actual speed.
+*/
+
+const speedWarning = (limit, speed) => {
+  if (speed > limit) {
+    return `your speed is ${speed} mph,slow down because you are exceeding the speed limit which is ${limit} mph`;
+  }
+};
+console.log(speedWarning(30, 40));
+
+//CHALLENGE FOUR: inline-arrow-function
+
+const distanceTraveledMiles = [267, 345, 234, 190, 299];
+
+//const distanceTraveledKm = distanceTraveledMiles.map(function(distance){
+//  return Math.round(distance * 1.6)
+//})
+
+//console.log(distanceTraveledKm)
+
+/*
+Challenge
+1. Refactor this .map method so the inline function is
+   an arrow function.
+2. Write the least amount of code possible.
+*/
+
+const distanceTraveledKm = distanceTraveledMiles.map((distance) =>
+  Math.round(distance * 1.6)
+);
+console.log(distanceTraveledKm);
